e985e9750c86282d02539ec2a03a3dd374742547
GY-GZW <2373460868@qq.com>
2025-05-03 12:38:41 +0800

    可爱的彩蛋

---
+++ caidan.js
@@ -0,0 +1,4 @@
+const acss = "\n  color: #1772F6;\n  font-weight: bold;\n  font-size: 3em;\n  padding: 10px 0;\n  text-shadow: 0.7px 1px 0 rgb(255 255 255 / 100%),\n    1.4px 2px 0 rgb(255 255 255 / 96%),\n    2.1px 3px 0 rgb(255 255 255 / 92%),\n    2.8px 4px 0 rgb(255 255 255 / 88%),\n    1px 1px 2px rgb(100 100 100 / 70%);\n";
+window.console.log("%c果园工作室", acss);
+const bcss = "\n  font-size: 1.2em;\n";
+window.console.log("%c嘿，大傻春，你看NM代码呢🤔，故障了就给我们TM发信息👇\n\n2373460868@qq.com\n\n没事看我们代码肯定TM没安好心", bcss);
